4b4fb44048c4781037b2d90046c2541c9a9e15cf
muflihun <muflihun@firmanedam19@gmail.com>
2020-04-15 09:14:04 +0700

    CRUD Pertanyaan

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $guarded = [];
     public function getRouteKeyName()
     {
         return 'slug';
@@ -22,5 +23,9 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    public function getPathAtribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
