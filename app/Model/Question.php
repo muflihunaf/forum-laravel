ae72922861009bb34ae0e2aa344ff9a4157ede98
muflihun <muflihun@firmanedam19@gmail.com>
2020-04-15 09:45:27 +0700

    Update

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -24,7 +24,7 @@ class Question extends Model
     {
         return $this->belongsTo(Category::class);
     }
-    public function getPathAtribute()
+    public function getPathAttribute()
     {
         return asset("api/question/$this->slug");
     }
